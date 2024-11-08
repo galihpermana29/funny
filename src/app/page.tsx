b8e4829d7487a5293d03f55b6b27174a71a9e69d
galihpermana29 <galihcbn123@gmail.com>
2024-11-08 23:17:26 +0700

    feat: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+'use client';
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 export default function Home() {
-  return <div>testing antd es</div>;
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      (audioRef.current as any).play();
+    }
+  }, []);
+  return (
+    <div>
+      <audio autoPlay loop ref={audioRef}>
+        <source
+          src={
+            'https://res.cloudinary.com/braiwjaya-university/video/upload/v1731082428/olpa_obssce.mp3'
+          }
+          type="audio/mpeg"
+        />
+      </audio>
+
+      <Image
+        src={
+          'https://res.cloudinary.com/braiwjaya-university/image/upload/v1731082430/olpa_kxopsy.jpg'
+        }
+        alt="olpa"
+        width={100}
+        height={100}
+        className="w-full h-screen"
+      />
+    </div>
+  );
 }
